e070e4ab009bfb042ea6b69d09618cd57056edbb
ricsoh <ricsoh@gmail.com>
2020-12-07 10:36:30 +0800

    For loop method

--- App.js
+++ App.js
@@ -2,14 +2,49 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const names = [
+  "Aaran",
+  "Aaren",
+  "Aarez",
+  "Aarman",
+  "Aaron",
+  "Aaron-James",
+  "Aarron",
+  "Aaryan",
+  "Aaryn",
+  "Aayan",
+  "Aazaan",
+  "Abaan",
+  "Abbas",
+  "Abdallah",
+  "Abdalroof",
+  "Abdihakim",
+  "Abdirahman",
+  "Abdisalam",
+  "Abdul",
+  "Abdul-Aziz",
+  "Abdulbasir",
+  "Abdulkadir",
+  "Abdulkarem",
+];
+
 export default function App() {
+  function displayNames() {
+    let nameJSX = [];
+    let name;
+    for (name of names) {
+      nameJSX.push(<Text>{name}</Text>);
+    }
+    return nameJSX;
+  }
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {displayNames()}
     </View>
   );
-}
+ }
+    
 
 const styles = StyleSheet.create({
   container: {
